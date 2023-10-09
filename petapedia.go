16f0b5bcfcc380f765bdcded258f637e4b037acc
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-09 19:12:51 +0700

    add peta dan peda

---
+++ petapedia.go
@@ -0,0 +1,21 @@
+package petapedia
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/petapedia/peda"
+)
+
+func init() {
+	functions.HTTP("PetaPedia", petaPedia)
+}
+
+func petaPedia(w http.ResponseWriter, r *http.Request) {
+	mconn := peda.SetConnection("MONGOULBI", "petapedia")           //dbname : petapedia
+	datagedung := peda.GetAllBangunanLineString(mconn, "petapedia") //collection name : petapedia
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(datagedung)
+}
