9428ced69767faf6c1b471be365592fd5871c2e0
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-15 06:14:18 +0700

    post

--- post/function.go
+++ post/function.go
@@ -6,16 +6,10 @@ import (
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-	"github.com/petapedia/peda"
 )
 
 func init() {
-	functions.HTTP("PetaPedia", petaPediaGet)
-}
-
-func petaPediaGet(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "https://jscroot.github.io")
-	fmt.Fprintf(w, peda.GCFHandler("MONGOULBI", "petapedia", "petapedia"))
+	functions.HTTP("PetaPedia", petaPediaPost)
 }
 
 func petaPediaPost(w http.ResponseWriter, r *http.Request) {
